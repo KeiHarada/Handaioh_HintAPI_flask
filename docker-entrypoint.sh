fd8cdda665133874006d188e95d36500ab3fce04
Kei Harada <harada.kei@ist.osaka-u.ac.jp>
2018-12-07 13:58:13 +0900

    change docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -f "app/runserver.py" ]
+if [ ! -f "/app/runserver.py" ]
 then
-	echo "app/runserver.py is not there."
+	echo "/app/runserver.py is not there."
 fi
 
 exec python3 runserver.py $NEO4J_USER $NEO4J_PASS $NEO4J_HOST
